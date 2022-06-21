833fff3aabb33d205ca9d9aafcd65323835bb5f2
Alexander4649 <handyasu08@gmail.com>
2022-06-21 13:26:19 +0000

    [Add]投稿順序切替実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -21,6 +21,16 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
   validates :star,presence:true
+ 
+  scope :latest, -> {order(created_at: :desc)} 
+  # scope :old, -> {order(created_at: :asc)}
+  scope :star_count, -> {order(star: :desc)}
+# order・・・データの取り出し
+# Latest・・・任意の名前で定義する
+# order(created_at: :desc)
+# created_at・・・投稿日のカラム
+# desc・・・昇順
+# asc・・・降順
   
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
