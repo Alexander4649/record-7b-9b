e57207271998b02293d474b087be9e410f2f7298
Alexander4649 <handyasu08@gmail.com>
2022-06-23 01:11:09 +0000

    [Add]タグ機能実装まではせず、必要な時に使用

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_022655) do
+ActiveRecord::Schema.define(version: 2022_06_23_005157) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
